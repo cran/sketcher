201ceee6824bed4ae96a157eb8312a8bf36ca17b
Hiroyuki Tsuda <tsuda16k@gmail.com>
2020-05-25 08:10:02 +0000

    version 0.1.3

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' A face image.
+#'
+#' A photograph obtained from a free stock photos site.
+#' pexels.com/photo/man-about-to-touch-his-face-wearing-blue-suit-718261/
+#'
+#' @format An array with 600 x 460 * 3 dimensions.
+#' Each dimension represents y-coordinate, x-coordinate, and color channel.
+#' @examples
+#' plot(face)
+"face"
